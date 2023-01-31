af136b0c2242fb4ac8ceb6c6a035af1db622943b
Tarunkumarchelumalla <70788951+Tarunkumarchelumalla@users.noreply.github.com>
2023-01-31 13:22:37 +0530

    2 files modified

--- src/app/hero-form/hero-form.component.ts
+++ src/app/hero-form/hero-form.component.ts
@@ -23,8 +23,23 @@ export class HeroFormComponent implements OnInit {
     // this.Employee.get('Name').valueChanges.subscribe((val) => {
     //   // this.length = val.length;
     // });
-    this.Employee.valueChanges.subscribe((val) => {
-      console.log(JSON.stringify(val));
+    // this.Employee.valueChanges.subscribe((val) => {
+    //   console.log(JSON.stringify(val));
+    // });
+  }
+  // this is importatnt
+  logkey(group: FormGroup): void {
+    Object.keys(group.controls).forEach((key: string) => {
+      const abstractcontrol = group.get(key);
+      if (abstractcontrol instanceof FormGroup) {
+        this.logkey(abstractcontrol);
+        abstractcontrol.disable();
+      } else {
+        console.log('Key =' + key + 'Value =' + abstractcontrol.value);
+      }
     });
+  // }
+  onclick(): void {
+    this.logkey(this.Employee);
   }
 }
